abc4818a174910f43e7c009621f86fea4a3c733a
Jasper van Santen <vansanten@gmail.com>
2016-04-17 15:47:51 +0200

    split index2.html

---
+++ html/old/javascript/application.js
@@ -0,0 +1,53 @@
+console.log("Start the application.")
+
+"use strict";
+// var http = require("http");
+//
+// var fs = require("fs");
+//
+// http.createServer(
+//    function (request, response) {
+//
+//       // Send the HTTP header
+//       // HTTP Status: 200 : OK
+//       // Content Type: text/plain
+//       response.writeHead(200, {'Content-Type': 'text/html'});
+//       response.write('<html>');
+//       var data = fs.readFileSync('test.txt');
+//       response.write('<p>'+data+'</p>');
+//       response.write('</html>');
+//
+//       // Send the response body as "Hello World"
+//       response.end('<p>Hello World\n</p>');
+//    }
+// ).listen(8081);
+//
+// // Console will print the message
+// console.log('Server running at http://127.0.0.1:8081/');
+
+
+// class MediocreDocument {
+   // constructor( fN ) {
+      // this.fileName = fN;
+      // var fs = require("fs");
+      // var data = fs.readFileSync( this.fileName );
+   // }
+   // function save(){
+   //    document.getElementById("justAsimpleTest").innerHTML = "Asavoviallylyly";
+   // }
+
+// }
+
+function MediocreBackEnd( fN ) {
+   // constructor( fN ) {
+      this.fileName = fN;
+   //    // var fs = require("fs");
+   //    // var data = fs.readFileSync( this.fileName );
+   // }
+   // function save(){
+   //    document.getElementById("justAsimpleTest").innerHTML = "Asavoviallylyly";
+   // }
+
+}
+MediocreBackEnd.prototype.
+// var myBE = new class MediocreBackEnd( 'test.txt' );
